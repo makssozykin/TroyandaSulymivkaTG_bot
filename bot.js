d8b7de7ba3c0c64e4e040e916d167fd9a74effbe
makssozykin <127692421+makssozykin@users.noreply.github.com>
2024-06-21 21:36:57 +0300

    test bot

--- bot.js
+++ bot.js
@@ -12,40 +12,47 @@ const chatIgTestGroup = -1002215119359;
 const bot = new Telegraf(token);
 
 const ip = '91.226.254.100';
-const ip2 = '91.226.254.118';
-const ip3 = '192.168.0.196';
+const ip2 = '192.168.0.1';
+const ip3 = '91.226.254.118';
+const ip4 = '10.118.52.103';
 let pingsArrOn = [];
 let pingsArrOff = [];
 
 const PingRequest = async () => {
-    const url = `http://ip-api.com/json/${ip}`;
+    const url = `http://ip-api.com/json/${ip2}`;
     try {
         const response = await axios.get(url);
+        console.log(response.data);
         const stat = response.data.status;
         let data = new Date().getTime();
         let pingObj = {
             status: stat,
             date: data
         }
-        const lightOn = 'Світло є';
+        const lightOn = 'Електропостачання відновлено. Світла не було ';
         const lightOff = 'Світло відсутнє';
         const sendToChannelOn = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatIgTestGroup}&text=${lightOn}`;
         const sendToChannelOff = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatIgTestGroup}&text=${lightOff}`;
         if (response.data.status === 'success') {
         
-            pingsArrOn.slice(1);
+            // pingsArrOn.slice(1);
             pingsArrOn.push(pingObj);
             const diff = pingsArrOn[pingsArrOn.length - 1].date - pingsArrOn[0].date;
+            msecToString(diff);
             await axios.get(sendToChannelOn);
-            console.log(pingsArrOn, diff);
+            console.log(pingsArrOn, msecToString(diff));
         }
         else {
             data = new Date().getTime();
-            pingsArrOff.slice(1);
+            // pingsArrOff.slice(1);
             pingsArrOff.push(pingObj);
+            // const differ = pingsArrOff[0].date - pingsArrOn[0].date;
+            // msecToString(differ);
             await axios.get(sendToChannelOff);
             console.log(pingsArrOff);
         }
+        
+        
     } catch (e) { 
         data = new Date().getTime();
         pingsArrOff.slice(1);
@@ -54,7 +61,7 @@ const PingRequest = async () => {
             date: data
         }
             pingsArrOff.push(pingObj);
-            await axios.get(sendToChannelOff);
+            // await axios.get(sendToChannelOff);
             console.log(pingsArrOff);
     }
 }
@@ -62,6 +69,14 @@ const PingRequest = async () => {
 // setInterval(PingRequest, delay);
 PingRequest();
 
+function msecToString(val) {
+  let mins = Math.round(val / 60000);
+  const hours = Math.floor(mins / 60);
+  mins %= 60;
+  if (mins < 10)
+    mins = '0' + mins;
+  return `${hours} год ${mins} хв`;
+}
 
 bot.start((ctx) => ctx.reply('Welcome!'));
 
